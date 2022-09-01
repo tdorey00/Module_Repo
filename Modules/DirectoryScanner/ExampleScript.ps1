6c8cc8638cad892d9e00952f3014744ac5bf0ff6
tdorey00 <tdorey00@gmail.com>
2022-08-31 22:17:14 -0400

    Testing...

--- Modules/DirectoryScanner/ExampleScript.ps1
+++ Modules/DirectoryScanner/ExampleScript.ps1
@@ -1,2 +1,8 @@
 Import-Module -Name $PSScriptRoot\DirectoryScanner -Force
-Get-Help -Name "Get-FileExtensionSizeList" -Detailed
+#Get-Help -Name "Get-FileExtensionSizeList" -Detailed
+$files = Get-ChildItem -Path "C:/Users/Bossman/Documents"
+foreach($file in $files){
+    if($file.Attributes -ieq "Archive"){
+        Write-Host "$($file.BaseName): $($file.Length)"
+    }
+}
