f71523d04ed02a288fe480a18fe7e8d7021338d4
tdorey00 <tdorey00@gmail.com>
2023-11-01 20:32:18 -0400

    Working on Extensions Filter and better file parse

--- Modules/DirectoryScanner/DirectoryScanner.psm1
+++ Modules/DirectoryScanner/DirectoryScanner.psm1
@@ -32,10 +32,12 @@ None. You cannot pipe objects to Get-FileExtensionSizeList.
 System.Collections.Hashtable. Get-FileExtensionSizeList returns a hashtable with the following format:
 {
     "EXTENSION(String)":{
+        "numberOfFiles": INT,
         "byteSize": INT,
         "percentageOfTotal": DOUBLE
     },
     "EXTENSION(String)":{
+        "numberOfFiles": INT,
         "byteSize": INT,
         "percentageOfTotal": DOUBLE
     }
@@ -88,6 +90,13 @@ https://github.com/tdorey00/PowerShell_Zone/tree/main/Modules/DirectoryScanner
     [bool]$parseAll = $Extensions.Count -eq 0
 
     Write-Host $files.Count
+
+    [int]$totalSize = 0
+
+    if(!$parseAll){
+        $files = $files | Where-Object {$Extensions -contains $_.Extension}
+    }
+    return $files    
     ##GROUNDWORK IS LAID MAKE IT WORK FOR ALL EXTENSIONS... THEN WE BUILD THE HASHTABLE prob should make it its own helper function
     #$filtered = $files.Where({$_.EXTENSION -eq ".zip"}, 0)
     #foreach($file in $filtered){
@@ -96,7 +105,46 @@ https://github.com/tdorey00/PowerShell_Zone/tree/main/Modules/DirectoryScanner
 
 }
 
-function script:Get-DirectoryValidity{
+function Add-PrimitiveTypesToHashtable {
+    #Helper Function
+    #Will add values to existings keys if it exists otherwise appends it to currently existing keys
+    #ONLY USE THIS WITH PRIMITIVE TYPES!!!!!
+    [cmdletBinding(PositionalBinding=$false)]
+    param (
+        #Table that will have key added
+        [Parameter(Mandatory=$true)]
+        [hashtable]$ExistingTable,
+        #Values to be added to key
+        [Parameter(Mandatory=$true)]
+        [hashtable]$ValuesToAdd,
+        #Key to be added to table
+        [Parameter(Mandatory=$true)]
+        [string]$Key
+    )
+
+    #If key does not exist add it and return existing table
+    if(!$ExistingTable.ContainsKey($Key)){
+        $ExistingTable[$Key] =  $ValuesToAdd
+    }
+    else{
+        #Copy to prevent modifying during iteration
+        $copy = $ValuesToAdd.Clone()
+
+        foreach($hash in $copy.Keys){
+
+            if($ExistingTable[$Key].ContainsKey($hash)){ #If key exists add it to existing key
+                $ExistingTable.$Key.$hash += $ValuesToAdd.$hash
+            }
+            else{ #If key doesn't exist add it
+                $ExistingTable[$Key].Add($hash, $ValuesToAdd.$hash)
+            }
+        } 
+    }
+
+    return $ExistingTable
+}
+
+function Get-DirectoryValidity{
     # Helper Function
     # Checks Directory Validity: if its a valid path, if it given path actually exists, and makes sure path is a directory not a file
     [cmdletBinding(PositionalBinding=$false)]
@@ -110,7 +158,9 @@ function script:Get-DirectoryValidity{
         throw "$(Get-Date) - $($MyInvocation.MyCommand) - Provided Directory '$($Path)' is not valid please check the path and try again."
     }
     elseif(-not (Test-Path -Path $Path -PathType Container)){ #Check if path is a Directory or a file
-        throw "$(Get-Date) - $($MyInvocation.MyCommand) - Provided Path '$($Path)' is not a valid Directory or is a file path please enter a valid Directory and not a file path."
+        throw "$(Get-Date) - $($MyInvocation.MyCommand) - Provided Path '$($Path)' is not a valid Directory or is a file path please enter a valid Directory."
     }
 
 }
+
+Export-ModuleMember -Function Get-FileExtensionSizeList
--- Modules/DirectoryScanner/ExampleScript.ps1
+++ Modules/DirectoryScanner/ExampleScript.ps1
@@ -2,6 +2,9 @@ Import-Module -Name $PSScriptRoot\DirectoryScanner -Force
 #Get-Help -Name "Get-FileExtensionSizeList" -Full
 
 #Test-Path -Path "C:\JavaWorkspace\.metadata\.lock" -PathType Container -IsValid
-Get-FileExtensionSizeList -Directory "..\..\..\Users\Bossman\Documents" 
+#Get-FileExtensionSizeList -Directory "..\..\..\Users\Bossman\Documents" -Verbose
+
+$files = Get-FileExtensionSizeList -Directory "..\..\..\..\..\Users\Bossman\Documents" -Extensions @('.cs', '.txt', '.png')
+Write-Host $files.Count
 
 
