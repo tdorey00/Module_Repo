903a922dccfd89b46edcadd32523f42b7864e0ae
tdorey00 <tdorey00@gmail.com>
2024-02-24 03:30:00 -0500

    Update the Module Analyzer for local analysis

--- Analyzers/ModuleAnalyzer.ps1
+++ Analyzers/ModuleAnalyzer.ps1
@@ -1,2 +1,2 @@
-$scanResult = Invoke-ScriptAnalyzer -Path $PSScriptRoot\..\Modules -Recurse -IncludeDefaultRules -ReportSummary -EnableExit
-Write-Host "Hello"
+#Run this to analyize the Modules Folder for Violations before committing
+Invoke-ScriptAnalyzer -Path $PSScriptRoot\..\Modules -Recurse -IncludeDefaultRules -ReportSummary
